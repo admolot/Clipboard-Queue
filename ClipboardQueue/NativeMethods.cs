7bb5f683cf747634302c6735f00255bd9f107dc1
admolot <admolot@gmail.com>
2026-08-04 09:49:35 +0300

    Update NativeMethods.cs

--- ClipboardQueue/NativeMethods.cs
+++ ClipboardQueue/NativeMethods.cs
@@ -9,6 +9,10 @@ internal static class NativeMethods
 
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool AddClipboardFormatListener(IntPtr hwnd);
+    
+    [DllImport("user32.dll")]
+    public static extern uint GetClipboardSequenceNumber();
+
 
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool RemoveClipboardFormatListener(IntPtr hwnd);
