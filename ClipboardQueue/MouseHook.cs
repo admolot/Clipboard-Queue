55c96b474320ee22b5d1543a5a6e0890b38ed164
admolot <admolot@gmail.com>
2026-09-01 20:27:35 +0300

    Update MouseHook.cs

--- ClipboardQueue/MouseHook.cs
+++ ClipboardQueue/MouseHook.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace ClipboardQueue;
 
@@ -14,8 +13,6 @@ internal sealed class MouseHook : IDisposable
     private const int WM_RBUTTONUP = 0x0205;
     private const int WM_MBUTTONDOWN = 0x0207;
     private const int WM_XBUTTONDOWN = 0x020B;
-
-    private const string MenuWindowClass = "#32768";
 
     private delegate IntPtr LowLevelMouseProc(int nCode, IntPtr wParam, IntPtr lParam);
 
@@ -37,31 +34,9 @@ internal sealed class MouseHook : IDisposable
         IntPtr wParam,
         IntPtr lParam);
 
-    [DllImport("user32.dll")]
-    private static extern IntPtr WindowFromPoint(POINT pt);
-
-    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
-    private static extern int GetClassName(IntPtr hWnd, StringBuilder lpClassName, int nMaxCount);
-
-    [StructLayout(LayoutKind.Sequential)]
-    private struct POINT
-    {
-        public int x;
-        public int y;
-    }
-
     private readonly LowLevelMouseProc _proc;
     private readonly IntPtr _hookId;
     private bool _disposed;
-
-    private DateTime _lastRightButtonUp = DateTime.MinValue;
-
-    /// <summary>
-    /// Fired when the user left-clicks shortly after a right click.
-    /// Arguments: clipboard sequence at click time, and whether the click
-    /// landed on a real system context menu (window class "#32768").
-    /// </summary>
-    public Action<uint, bool>? LeftClickAfterRightClick { get; set; }
 
     public MouseHook()
     {
@@ -87,37 +62,15 @@ internal sealed class MouseHook : IDisposable
             {
                 int msg = (int)wParam;
 
-                if (msg == WM_RBUTTONUP)
+                if (msg == WM_LBUTTONDOWN)
                 {
-                    _lastRightButtonUp = DateTime.UtcNow;
-                    InputActivity.NoteRightButtonUp();
-                    InputActivity.Note();
+                    // Any left click can precede a paste (menu choice etc.).
+                    InputActivity.NoteGesture();
                 }
-                else if (msg == WM_LBUTTONDOWN)
-                {
-                    if ((DateTime.UtcNow - _lastRightButtonUp).TotalSeconds < 10)
-                    {
-                        bool menuWindow = IsMenuWindowUnderCursor(lParam);
-
-                        if (menuWindow)
-                        {
-                            InputActivity.NoteGesture();
-                        }
-                        else
-                        {
-                            InputActivity.Note();
-                        }
-
-                        LeftClickAfterRightClick?.Invoke(
-                            NativeMethods.GetClipboardSequenceNumber(),
-                            menuWindow);
-                    }
-                    else
-                    {
-                        InputActivity.Note();
-                    }
-                }
-                else if (msg == WM_MBUTTONDOWN || msg == WM_XBUTTONDOWN)
+                else if (msg == WM_RBUTTONDOWN ||
+                         msg == WM_RBUTTONUP ||
+                         msg == WM_MBUTTONDOWN ||
+                         msg == WM_XBUTTONDOWN)
                 {
                     InputActivity.Note();
                 }
@@ -129,32 +82,6 @@ internal sealed class MouseHook : IDisposable
         }
 
         return CallNextHookEx(_hookId, nCode, wParam, lParam);
-    }
-
-    private static bool IsMenuWindowUnderCursor(IntPtr lParam)
-    {
-        try
-        {
-            POINT pt;
-            pt.x = Marshal.ReadInt32(lParam, 0);
-            pt.y = Marshal.ReadInt32(lParam, 4);
-
-            IntPtr hwnd = WindowFromPoint(pt);
-
-            if (hwnd == IntPtr.Zero)
-                return false;
-
-            var sb = new StringBuilder(32);
-
-            if (GetClassName(hwnd, sb, sb.Capacity) == 0)
-                return false;
-
-            return sb.ToString() == MenuWindowClass;
-        }
-        catch
-        {
-            return false;
-        }
     }
 
     public void Dispose()
